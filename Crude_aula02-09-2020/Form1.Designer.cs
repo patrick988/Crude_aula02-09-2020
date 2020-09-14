786f2d3fa51afade0e1916546ad8f83a7a06c380
Patrick <patrick.tecjord@gmail.com>
2020-09-14 20:22:13 -0300

    Primeira atualizacao di projeto

---
+++ Crude_aula02-09-2020/Form1.Designer.cs
@@ -0,0 +1,186 @@
+﻿namespace Crude_aula02_09_2020
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblNome = new System.Windows.Forms.Label();
+            this.lblTelefone = new System.Windows.Forms.Label();
+            this.textname = new System.Windows.Forms.TextBox();
+            this.textelefone = new System.Windows.Forms.TextBox();
+            this.btninserir = new System.Windows.Forms.Button();
+            this.btnAlterar = new System.Windows.Forms.Button();
+            this.btnExcluir = new System.Windows.Forms.Button();
+            this.lblid = new System.Windows.Forms.Label();
+            this.textid = new System.Windows.Forms.TextBox();
+            this.btnPesquisa = new System.Windows.Forms.Button();
+            this.dgagenda = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dgagenda)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblNome
+            // 
+            this.lblNome.AutoSize = true;
+            this.lblNome.Location = new System.Drawing.Point(164, 68);
+            this.lblNome.Name = "lblNome";
+            this.lblNome.Size = new System.Drawing.Size(35, 13);
+            this.lblNome.TabIndex = 0;
+            this.lblNome.Text = "Nome";
+            this.lblNome.Click += new System.EventHandler(this.lblNome_Click);
+            // 
+            // lblTelefone
+            // 
+            this.lblTelefone.AutoSize = true;
+            this.lblTelefone.Location = new System.Drawing.Point(150, 94);
+            this.lblTelefone.Name = "lblTelefone";
+            this.lblTelefone.Size = new System.Drawing.Size(49, 13);
+            this.lblTelefone.TabIndex = 1;
+            this.lblTelefone.Text = "Telefone";
+            this.lblTelefone.Click += new System.EventHandler(this.lblTelefone_Click);
+            // 
+            // textname
+            // 
+            this.textname.Location = new System.Drawing.Point(205, 68);
+            this.textname.Name = "textname";
+            this.textname.Size = new System.Drawing.Size(223, 20);
+            this.textname.TabIndex = 2;
+            // 
+            // textelefone
+            // 
+            this.textelefone.Location = new System.Drawing.Point(205, 94);
+            this.textelefone.Name = "textelefone";
+            this.textelefone.Size = new System.Drawing.Size(143, 20);
+            this.textelefone.TabIndex = 2;
+            // 
+            // btninserir
+            // 
+            this.btninserir.Location = new System.Drawing.Point(234, 355);
+            this.btninserir.Name = "btninserir";
+            this.btninserir.Size = new System.Drawing.Size(75, 23);
+            this.btninserir.TabIndex = 3;
+            this.btninserir.Text = "Inserir";
+            this.btninserir.UseVisualStyleBackColor = true;
+            this.btninserir.Click += new System.EventHandler(this.btninserir_Click);
+            // 
+            // btnAlterar
+            // 
+            this.btnAlterar.Location = new System.Drawing.Point(311, 355);
+            this.btnAlterar.Name = "btnAlterar";
+            this.btnAlterar.Size = new System.Drawing.Size(75, 23);
+            this.btnAlterar.TabIndex = 4;
+            this.btnAlterar.Text = "Alterar";
+            this.btnAlterar.UseVisualStyleBackColor = true;
+            this.btnAlterar.Click += new System.EventHandler(this.btnAlterar_Click);
+            // 
+            // btnExcluir
+            // 
+            this.btnExcluir.Location = new System.Drawing.Point(392, 355);
+            this.btnExcluir.Name = "btnExcluir";
+            this.btnExcluir.Size = new System.Drawing.Size(75, 23);
+            this.btnExcluir.TabIndex = 5;
+            this.btnExcluir.Text = "Excluir";
+            this.btnExcluir.UseVisualStyleBackColor = true;
+            this.btnExcluir.Click += new System.EventHandler(this.btnExcluir_Click);
+            // 
+            // lblid
+            // 
+            this.lblid.AutoSize = true;
+            this.lblid.Location = new System.Drawing.Point(164, 43);
+            this.lblid.Name = "lblid";
+            this.lblid.Size = new System.Drawing.Size(18, 13);
+            this.lblid.TabIndex = 6;
+            this.lblid.Text = "ID";
+            this.lblid.Click += new System.EventHandler(this.lblid_Click);
+            // 
+            // textid
+            // 
+            this.textid.Location = new System.Drawing.Point(205, 40);
+            this.textid.Name = "textid";
+            this.textid.Size = new System.Drawing.Size(100, 20);
+            this.textid.TabIndex = 7;
+            this.textid.Text = "0";
+            this.textid.TextChanged += new System.EventHandler(this.textid_TextChanged);
+            // 
+            // btnPesquisa
+            // 
+            this.btnPesquisa.Location = new System.Drawing.Point(311, 40);
+            this.btnPesquisa.Name = "btnPesquisa";
+            this.btnPesquisa.Size = new System.Drawing.Size(75, 23);
+            this.btnPesquisa.TabIndex = 8;
+            this.btnPesquisa.Text = "Pesquisa";
+            this.btnPesquisa.UseVisualStyleBackColor = true;
+            this.btnPesquisa.Click += new System.EventHandler(this.btnPesquisa_Click);
+            // 
+            // dgagenda
+            // 
+            this.dgagenda.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgagenda.Location = new System.Drawing.Point(167, 120);
+            this.dgagenda.Name = "dgagenda";
+            this.dgagenda.Size = new System.Drawing.Size(377, 206);
+            this.dgagenda.TabIndex = 9;
+            this.dgagenda.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgagendaclick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(837, 483);
+            this.Controls.Add(this.dgagenda);
+            this.Controls.Add(this.btnPesquisa);
+            this.Controls.Add(this.textid);
+            this.Controls.Add(this.lblid);
+            this.Controls.Add(this.btnExcluir);
+            this.Controls.Add(this.btnAlterar);
+            this.Controls.Add(this.btninserir);
+            this.Controls.Add(this.textelefone);
+            this.Controls.Add(this.textname);
+            this.Controls.Add(this.lblTelefone);
+            this.Controls.Add(this.lblNome);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dgagenda)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblNome;
+        private System.Windows.Forms.Label lblTelefone;
+        private System.Windows.Forms.TextBox textname;
+        private System.Windows.Forms.TextBox textelefone;
+        private System.Windows.Forms.Button btninserir;
+        private System.Windows.Forms.Button btnAlterar;
+        private System.Windows.Forms.Button btnExcluir;
+        private System.Windows.Forms.Label lblid;
+        private System.Windows.Forms.TextBox textid;
+        private System.Windows.Forms.Button btnPesquisa;
+        private System.Windows.Forms.DataGridView dgagenda;
+    }
+}
+
---
+++ Crude_aula02-09-2020/Form1.cs
@@ -0,0 +1,241 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.Drawing.Text;
+
+namespace Crude_aula02_09_2020
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+       
+        }
+
+        SqlConnection sqlCon = null;
+        private string strCon = @"Password=papa1212;Persist Security Info=True;User ID=sa;Initial Catalog=BDCrud;Data Source=DESKTOP-O0QHQ28\SQLEXPRESS";
+            private string strSql = string.Empty;
+
+
+        private void lblNome_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btninserir_Click(object sender, EventArgs e)
+        {
+
+            strSql = "insert into Agenda  (nome, Telefone) values (@Nome, @Telefone)";
+
+            sqlCon = new SqlConnection(strCon);
+
+            SqlCommand comando = new SqlCommand(strSql, sqlCon);
+
+            try
+            {
+                sqlCon.Open();
+                //passagem de parametros
+                comando.Parameters.Add("@Nome", SqlDbType.VarChar).Value = textname.Text;
+                comando.Parameters.Add("@Telefone", SqlDbType.VarChar).Value = textelefone.Text;
+
+                comando.ExecuteNonQuery();
+                MessageBox.Show("Registro efetuado com sucesso");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                sqlCon.Close();
+                textname.Clear();
+                textelefone.Clear();
+                textname.Focus();
+
+            }
+
+
+
+
+        }
+
+        private void btnPesquisa_Click(object sender, EventArgs e)
+        {
+            if (textid.Text == string.Empty)
+            {
+                strSql = "select * from agenda order by id";
+                sqlCon = new SqlConnection(strCon);
+                SqlCommand comando = new SqlCommand(strSql, sqlCon);
+                SqlDataAdapter da = new SqlDataAdapter(strSql, sqlCon);
+                DataTable dt = new DataTable();
+                da.Fill(dt);
+                dgagenda.DataSource = dt;
+
+            }
+            else
+            {
+
+                strSql = "select * from agenda where id=@id";
+                sqlCon = new SqlConnection(strCon);
+                SqlCommand comando = new SqlCommand(strSql, sqlCon);
+
+                comando.Parameters.Add("@id", SqlDbType.Int).Value = textid.Text;
+
+
+                try
+                {
+
+
+                    if (textid.Text == string.Empty)
+                    {
+                        throw new Exception("Digite um ID");
+
+
+                    }
+
+                    sqlCon.Open();
+
+                    SqlDataReader dr = comando.ExecuteReader();
+
+
+                    if (dr.HasRows == false)
+                    {
+                        throw new Exception("Id não cadastrado");
+
+                    }
+
+                    if (dr.Read())
+                    {
+                        textid.Text = Convert.ToString(dr["id"]);
+                        textname.Text = Convert.ToString(dr["Nome"]);
+                        textelefone.Text = Convert.ToString(dr["Telefone"]);
+
+                    }
+
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+                finally
+                {
+                    sqlCon.Close();
+                    textname.Focus();
+                }
+
+
+            }
+
+        }
+
+        private void btnAlterar_Click(object sender, EventArgs e)
+        {
+            strSql = "update agenda set Nome=@Nome, Telefone=@Telefone where id=@id";
+            sqlCon = new SqlConnection(strCon);
+            SqlCommand comando = new SqlCommand(strSql, sqlCon);
+
+            comando.Parameters.Add("@id", SqlDbType.VarChar).Value = textid.Text;
+            comando.Parameters.Add("@Nome", SqlDbType.VarChar).Value = textname.Text;
+            comando.Parameters.Add("@Telefone", SqlDbType.VarChar).Value = textelefone.Text;
+
+
+            try
+            {
+                sqlCon.Open();
+                comando.ExecuteNonQuery();
+                MessageBox.Show("Cadastro atualizado!!");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                sqlCon.Close();
+                textname.Clear();
+                textelefone.Clear();
+                textname.Focus();
+
+                strSql = "select * from agenda order by id";
+                sqlCon = new SqlConnection(strCon);
+                SqlCommand comm = new SqlCommand(strSql, sqlCon);
+                SqlDataAdapter da = new SqlDataAdapter(strSql, sqlCon);
+                DataTable dt = new DataTable();
+                da.Fill(dt);
+                dgagenda.DataSource = dt;
+
+            }
+
+
+
+        }
+
+        private void lblid_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lblTelefone_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnExcluir_Click(object sender, EventArgs e)
+        {
+            // programa para deletar registros
+            if (MessageBox.Show("Deseja Excluir o registro?", "Cuidado", MessageBoxButtons.YesNo, MessageBoxIcon.Question, MessageBoxDefaultButton.Button2)==DialogResult.No)
+            {
+                MessageBox.Show("Operação Cancelada");
+            }
+            else
+            {
+                strSql = "Delete from agenda where id = @id";
+                sqlCon = new SqlConnection(strCon);
+
+                SqlCommand comando = new SqlCommand(strSql, sqlCon);
+                comando.Parameters.Add("@id", SqlDbType.Int).Value = textid.Text;
+
+                try
+                {
+                    sqlCon.Open();
+                    comando.ExecuteNonQuery();
+                    MessageBox.Show("Registro apagado");
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+                finally
+                {
+                    sqlCon.Close();
+                    textid.Text = "";
+                    textname.Clear();
+                    textelefone.Clear();
+                    textname.Focus();
+
+                }
+            }
+
+        }
+
+        private void dgagendaclick(object sender, DataGridViewCellEventArgs e)
+        {
+            textid.Text = dgagenda.CurrentRow.Cells[0].Value.ToString();
+            textname.Text = dgagenda.CurrentRow.Cells[1].Value.ToString();
+            textelefone.Text = dgagenda.CurrentRow.Cells[2].Value.ToString();
+        }
+
+        private void textid_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
